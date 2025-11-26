2cb71d05e2a4975f1fbe4db746e38a86ec062501
Derrick Smith <derricksmith@example.com>
2025-11-26 14:54:36 -0700

    Bump version to 1.0.1 for plugin update
    Updated version from 1.0.0 to 1.0.1 to allow import of updated plugin
    with corrected client_id field type (string instead of credential_secret_key).
    
    Changes:
    - plugin.spec.yaml: version 1.0.0 -> 1.0.1
    - Makefile: VERSION 1.0.0 -> 1.0.1
    - setup.py: version 1.0.0 -> 1.0.1
    - Built new plugin file: derricksmith_haloitsm_1.0.1.tar
    - Removed old version: derricksmith_haloitsm_1.0.0.tar

--- plugins/haloitsm/setup.py
+++ plugins/haloitsm/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="komand_haloitsm",
-    version="1.0.0",
+    version="1.0.1",
     description="HaloITSM plugin for Rapid7 InsightConnect",
     author="Rapid7",
     packages=find_packages(),
